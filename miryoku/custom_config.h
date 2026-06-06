f76189a1d4d3ef2a1ac7d2330c733d4878129708
srebl <128514485+srebl@users.noreply.github.com>
2026-06-06 22:05:19 +0200

    blub

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,7 +1,7 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#define MIRYOKU_ALPHAS_QWERTZ
-#define MIRYOKU_TAP_QWERTZ
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_TAP_QWERTY
 
 #define U_DE 9
